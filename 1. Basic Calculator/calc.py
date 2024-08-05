53c5d561c7302a7b94e7eff7fd98c50756126d77
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2024-08-05 20:46:20 +0400

    Add basic functions
    Added the addition, subtraction, multiplication
    and division functions

---
+++ 1. Basic Calculator/calc.py
@@ -0,0 +1,23 @@
+"""
+Creating a basic calculator
+"""
+
+def addition(a, b):
+    '''Add two numbers'''
+    ans = a + b
+    return ans
+
+def subtraction(a, b):
+    '''subtract two numbers'''
+    ans = a - b
+    return ans
+
+def multiplication(a, b):
+    '''multiply two numbers'''
+    ans = a * b
+    return ans
+
+def division(a, b):
+    '''divide two numbers'''
+    ans = a / b
+    return ans
