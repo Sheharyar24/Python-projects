489ec9d93b55a86353d9a07d3742cfd5c6a45fe3
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2024-08-09 02:36:22 +0400

    Add exception handling
    Added exception handling to the following cases:
    - user enters a number outside the range
    - user enters a string
    also changed the while loop from while True to Count > 0

--- 2. Number Guessing Game/game.py
+++ 2. Number Guessing Game/game.py
@@ -2,6 +2,9 @@
 
 import random
 
+class InvalidNumber(Exception):
+    '''Used when the user enters a number outside the range'''
+
 def num():
     '''Generates a random number'''
     x = random.randint(1,10)
@@ -9,7 +12,6 @@ def num():
 
 # Generate and store a number
 y = num()
-print(y) # for reference to see what the number is
 
 # The number of guesses the user has
 COUNT = 3
@@ -19,15 +21,28 @@ print("Welcome to the Guessing Game!")
 print(f"You have {COUNT} guesses!\n")
 
 
-while True:
-    guess = int(input("Guess the number (between 1 - 10): "))
+while COUNT > 0:
+    try:
+        guess = int(input("Guess the number (between 1 - 10): "))
+        if guess < 1 or guess > 10:
+            raise InvalidNumber
+    except InvalidNumber:
+        print("Please enter a number between 1 and 10")
+    except ValueError:
+        print("Please enter a number!")
 
     if guess == y:
         print(f"Well done!, You guessed it in {TRIES - COUNT} tries")
         break
-    if COUNT == 1:
-        print("You are out of attempts!")
-        break
     else:
         COUNT -= 1
-        print(f"You have {COUNT} remaining attempts")
+
+        if guess < y:
+            print("Too low!")
+        else:
+            print("Too high!")
+        
+        if COUNT > 0:
+            print(f"You have {COUNT} remaining attempts\n")
+        else:
+            print(f"You're out of attempts!, the number was {y}")
