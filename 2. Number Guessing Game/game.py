b3bfb433726af303277c925eba16de55c87fac4e
Sheharyar24 <sheharyarahmedsyed@gmail.com>
2024-08-09 02:50:12 +0400

    Add replay feature
    allows the user to play the game again with a new random number.

--- 2. Number Guessing Game/game.py
+++ 2. Number Guessing Game/game.py
@@ -10,39 +10,48 @@ def num():
     x = random.randint(1,10)
     return x
 
-# Generate and store a number
-y = num()
+def play():
+    '''Makes everything inside the function so that it can be played again'''
+    # Generate and store a number
+    y = num()
 
-# The number of guesses the user has
-COUNT = 3
-TRIES = COUNT + 1
+    # The number of guesses the user has
+    count = 3
+    tries = count + 1
 
-print("Welcome to the Guessing Game!")
-print(f"You have {COUNT} guesses!\n")
+    print("\nWelcome to the Guessing Game!")
+    print(f"You have {count} guesses!\n")
 
 
-while COUNT > 0:
-    try:
-        guess = int(input("Guess the number (between 1 - 10): "))
-        if guess < 1 or guess > 10:
-            raise InvalidNumber
-    except InvalidNumber:
-        print("Please enter a number between 1 and 10")
-    except ValueError:
-        print("Please enter a number!")
+    while count > 0:
+        try:
+            guess = int(input("Guess the number (between 1 - 10): "))
+            if guess < 1 or guess > 10:
+                raise InvalidNumber
+        except InvalidNumber:
+            print("Please enter a number between 1 and 10")
+        except ValueError:
+            print("Please enter a number!")
 
-    if guess == y:
-        print(f"Well done!, You guessed it in {TRIES - COUNT} tries")
-        break
-    else:
-        COUNT -= 1
+        if guess == y:
+            print(f"Well done!, You guessed it in {tries - count} tries")
+            break
+        else:
+            count -= 1
 
         if guess < y:
             print("Too low!")
         else:
             print("Too high!")
         
-        if COUNT > 0:
-            print(f"You have {COUNT} remaining attempts\n")
+        if count > 0:
+            print(f"You have {count} remaining attempts\n")
         else:
             print(f"You're out of attempts!, the number was {y}")
+
+while True:
+    play()
+    again = input("\nDo you want to play again? (yes/no): ").lower()
+    if again != 'yes':
+        print("Thank you for playing!")
+        break
